f6f4f61331b82a0ebf0504065aca27017db960fb
cnshnom <yupeiyu.cns.hn@gmail.com>
2023-10-15 15:12:20 +0200

    frontend typescript

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,10 @@
-import React from 'react';
-import { useState } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-
-const tasks = [
-  {
-    id: 1,
-    title: "go gym",
-    description: "train legs",
-    completed: false
-  },
-
-  {
-    id: 2,
-    title: "go supermarket",
-    description: "buy food",
-    completed: false
-  },
-  {
-    id: 3,
-    title: "learn django",
-    description: "build todo app",
-    completed: true
-  },
-]
-
-
-
 function App() {
-  const [viewCompleted, setViewCompleted] = useState(false);
-  const [taskList, setTaskList] = useState(tasks);
-  
-  function clickComplete() {
-    setViewCompleted(!clickComplete);
-  }
-  
-
-  
-
-
   return (
-
-    <div>
-      <h1 className="text-black text-uppercase text-center my-4">task manager</h1>
-      <div className="row ">
-        <div className="col-md-6 col-sm-10 mx-auto p-0">
-          <div className="card p-3">
-            <div>
-              <button className='btn btn-primary'>add task</button>
-            </div>
-            
-          </div>
-        </div>
-      </div>
+    <div className="App">
+     
     </div>
   );
 }
